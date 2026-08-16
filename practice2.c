a4c6daab0a5cff868ab1061a32547a7db01d7f84
Kodakanchi Bharadwaj <sunnybharadwaj177@gmail.com>
2026-08-16 17:43:01 +0530

    Create practice2.c

---
+++ practice2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf(" Name\t\t\t : KODAKANCHI BHARADWAJ ");
+	printf(" \nSchool name\t\t : ABC school ");
+	printf(" \nInter college\t\t : XYZ college ");
+	printf(" \nEngineering college\t : VNRVJIT ");
+	printf(" \nBranch\t\t\t : RAI ");
+	printf(" \nAcademic year\t\t : 2026-2030 ");
+}
