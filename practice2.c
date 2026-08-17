3848eca7d640bc19aef1a38ef8f009cd9237ccb6
Kodakanchi Bharadwaj <sunnybharadwaj177@gmail.com>
2026-08-17 07:30:32 +0530

    Update practice2.c

--- practice2.c
+++ practice2.c
@@ -4,7 +4,7 @@ void main()
 	printf(" Name\t\t\t : KODAKANCHI BHARADWAJ ");
 	printf(" \nSchool name\t\t : ABC school ");
 	printf(" \nInter college\t\t : XYZ college ");
-	printf(" \nEngineering college\t : VNRVJIT ");
+	printf(" \nEngineering college\t : VNRVJIET ");
 	printf(" \nBranch\t\t\t : RAI ");
 	printf(" \nAcademic year\t\t : 2026-2030 ");
 }
